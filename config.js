07fe78ed22773dad33279e13b50b927a225bb985
nick <nlomba1@gmail.com>
2021-02-16 16:07:25 -0500

    pg 283

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV,
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://283801@localhost/dnd_db',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
 };
