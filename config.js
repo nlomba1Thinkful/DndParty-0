6da3afaee7f50c3faaaf723605a139ef8e275328
nick <nlomba1@gmail.com>
2021-02-16 16:35:13 -0500

    uri

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL:
-    process.env.DATABASE_URL || 'postgresql://283801@localhost/dnd_db',
+    process.env.DATABASE_URL ||
+    'postgres://fhmngnyqldrgjq:8b72538f3dd33e9f18abbb5d33487ae5b76bed8455ab2a2d2b5ac24c2cd5e8a6@ec2-54-225-130-212.compute-1.amazonaws.com:5432/d575g45sd203t5',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY,
 };
